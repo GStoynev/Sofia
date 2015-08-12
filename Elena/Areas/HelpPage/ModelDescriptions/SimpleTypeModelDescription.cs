24b76c9228ea2895f6aee5891d1c8d22089981ac
GStoynev <gstoynev@qsystematic.com>
2015-08-11 22:06:11 -0400

    Initial commit of solution files

---
+++ Elena/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Elena.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Elena/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Elena.WebApiApplication" Language="C#" %>
