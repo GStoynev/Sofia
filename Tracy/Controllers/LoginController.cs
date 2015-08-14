4b8b7a59cf8151c6d012e710fcf27d4f6be8fffd
vkostov@mail.bg <Express2014>
2015-08-14 17:46:40 -0400

    I created LoginController

---
+++ Tracy/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Tracy.Controllers
+{
+    public class LoginController : ApiController
+    {
+        [HttpGet, ActionName("KajiZdrasti")]
+        public HttpResponseMessage SayHi()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "SUCCESS");
+        }
+    }
+}
