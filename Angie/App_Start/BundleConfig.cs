7e915e603c4745cc2d7f103e4058c2c030a062b1
GStoynev <gstoynev@qsystematic.com>
2015-11-27 21:36:57 -0500

    Angular2 working

--- Angie/App_Start/BundleConfig.cs
+++ Angie/App_Start/BundleConfig.cs
@@ -5,6 +5,17 @@ namespace Angie
 {
     public class BundleConfig
     {
+        public static void NewRegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
